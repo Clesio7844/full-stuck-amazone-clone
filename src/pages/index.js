feced6d20fd543fd0165933b60df8dce468133d2
david <daviddecastro42@live.co.uk>
2021-06-08 22:59:58 +0100

    finished the banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
+import ProductFeed from '../components/ProductFeed';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -12,7 +14,26 @@ export default function Home() {
 
       {/* header */}
       <Header />
-      {/* ---- ---- */}
+
+      <main className='max-w-screen-2xl mx-auto'>
+        {/* Banner */}
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
 }
+
+export async function getServerSideProps(contex) {
+  const products = await fetch('https://fakestoreapi.com/products').then(res =>
+    res.json()
+  );
+
+  return {
+    props: {
+      products: products
+    }
+  };
+}
+
+// GET >>> https://fakestoreapi.com/products

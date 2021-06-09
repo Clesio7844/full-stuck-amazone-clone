82a56bf45fa957da0d2538dce7747a1debc6726a
david <daviddecastro42@live.co.uk>
2021-06-09 01:01:09 +0100

    finished the google authention sing in and signout

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA79v6hhyxDVaNSfebdrRfz_XzJ6KxS2oU',
+  authDomain: 'e-2-yt.firebaseapp.com',
+  projectId: 'e-2-yt',
+  storageBucket: 'e-2-yt.appspot.com',
+  messagingSenderId: '617438775794',
+  appId: '1:617438775794:web:508d40bd3ce3a04d01436b'
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ import {
   ShoppingCartIcon
 } from '@heroicons/react/outline';
 
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 function Header() {
+  const [session] = useSession();
+
   return (
     <header>
       {/* top */}
@@ -32,8 +36,13 @@ function Header() {
 
         {/* right */}
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div className=' link'>
-            <p>Hello David De Castro</p>
+          <div
+            onClick={!session ? signIn : signOut}
+            className=' cursor-pointer link'
+          >
+            <p className='hover:underline'>
+              {session ? `Hello, ${session.user.name}` : 'Sign In'}
+            </p>
             <p className='font-extrabold  md:text-sm'>Account & Lists</p>
           </div>
           <div className=' link'>
